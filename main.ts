8d82c75cc984f8368784fcfff2e8b806a374cc20
Flavius Bejan <flaviusbejan96@yahoo.com>
2022-12-13 15:23:05 +0200

    done ex 4

--- main.ts
+++ main.ts
@@ -2,19 +2,14 @@
  * START: Follow the instructions below.
  */
 
-function getPopulation(): number {
-	return 69_950_850;
-}
+// Create a `Country` type alias for the `string` type.
+// Change the type for each variable to use the `Country` type.
+type Country = string;
+const country1: Country = "Nigeria";
 
-// Add a type assertion after the call to `getPopulation()`.
-// Hint: What's the actual type of the value returned by `getPopulation()`?
+const country2: Country = "Italy";
 
-const population: number = getPopulation();
-
-// Now you've added a type assertion, there's a type error in the code below.
-// Remove the code that is incorrect.
-
-console.log(population * 2);
+const country3: Country = "China";
 
 // ----
 
